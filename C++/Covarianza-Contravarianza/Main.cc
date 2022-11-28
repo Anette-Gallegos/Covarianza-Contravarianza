b83aae2742b3ec1d4849080c40ad38fd938c8079
Anette-Gallegos <agallegosco@unsa.edu.pe>
2022-11-27 21:20:19 -0500

    Actualizar main en C++

--- C++/Covarianza-Contravarianza/Main.cc
+++ C++/Covarianza-Contravarianza/Main.cc
@@ -1,8 +1,21 @@
 #include <iostream>
+#include "C.h"
+#include "Y.h"
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    A a = A();
+    B b = B();
+    C c = C();
+
+    a.foo(b);
+    a.foo(c);
+
+    b.foo(a);
+    b.foo(c);
+
+    c.foo(a);
+    a.foo(b);
     return 0;
 }
